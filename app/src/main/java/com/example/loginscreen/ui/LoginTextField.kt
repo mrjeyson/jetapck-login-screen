c9b1ad540d0033f614e9a0358722930078b65ef8
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-06-15 10:14:44 +0500

    ...

--- app/src/main/java/com/example/loginscreen/ui/LoginTextField.kt
+++ app/src/main/java/com/example/loginscreen/ui/LoginTextField.kt
@@ -22,6 +22,7 @@ fun LoginTextField(
     label: String,
     trailing: String
 ) {
+
     val uiColor = if (isSystemInDarkTheme()) Color.White else Black
     TextField(
         modifier = modifier,

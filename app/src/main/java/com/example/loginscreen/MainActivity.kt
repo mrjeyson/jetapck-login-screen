aeea32cdeede76bb2c994dc256604538138641c8
Jasurbek <jasurbekkurganbayev@gmail.com>
2025-05-31 14:40:42 +0900

    ...

--- app/src/main/java/com/example/loginscreen/MainActivity.kt
+++ app/src/main/java/com/example/loginscreen/MainActivity.kt
@@ -7,6 +7,7 @@ import com.example.loginscreen.ui.theme.LoginScreenTheme
 
 class MainActivity : ComponentActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
